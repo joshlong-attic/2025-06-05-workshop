9d69bc162f8163e859c51035c91238b6f9540d5b
Josh Long <54473+joshlong@users.noreply.github.com>
2025-06-07 12:42:30 +0900

    client

--- client/src/main/java/com/example/client/ClientApplication.java
+++ client/src/main/java/com/example/client/ClientApplication.java
@@ -3,10 +3,12 @@ package com.example.client;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
 
 import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions.rewritePath;
+import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions.uri;
 import static org.springframework.cloud.gateway.server.mvc.filter.TokenRelayFilterFunctions.tokenRelay;
 import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
 import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
@@ -19,32 +21,26 @@ public class ClientApplication {
     }
 
     @Bean
-    RouterFunction<ServerResponse> httpRoutes() {
-        return route()
+    @Order(2) // NB: this must be evaluated *after* the more specific api() bean
+    RouterFunction<ServerResponse> html() {
+        return route("html")
+                .GET("/**", http())
+                .before(uri("http://localhost:8020"))
                 .filter(tokenRelay())
-                .before(rewritePath("/api", "/"))
-                .GET("/api/**", http("http://localhost:8080/"))
-                .GET("/**", http("http://localhost:8020/"))
                 .build();
     }
 
-//    @Bean
-//    RouterFunction<ServerResponse> api() {
-//        return route("api")
-//                .GET("/api/**", http("http://localhost:8080"))
-////                .before(uri("http://localhost:8080"))
-//                .before(rewritePath("/api","/"))
-//                .filter(tokenRelay())
-//                .build();
-//    }
-//
-//    @Bean
-//    RouterFunction<ServerResponse> html() {
-//        return route("html")
-//                .GET("/**", http("http://localhost:8020"))
-////                .before(uri("http://localhost:8020"))
-//                .filter(tokenRelay())
-//                .build();
-//    }
-//
+
+    @Bean
+    @Order(1)
+    RouterFunction<ServerResponse> api() {
+        return route("api")
+                .GET("/api/**", http())
+                .before(rewritePath("/api", "/"))
+                .filter(tokenRelay())
+                .before(uri("http://localhost:8080"))
+                .build();
+    }
+
+
 }
